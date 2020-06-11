b32f74346736b40b008720775428ad088066f01f
Thor40 <nicholas.kosik@gmail.com>
2020-06-11 18:55:28 -0500

    posts/user

--- models/index.js
+++ models/index.js
@@ -1,4 +1,15 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations
+// Post model belongs to User model
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// post belongs to one user, but not many users
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 module.exports = { User, Post };
