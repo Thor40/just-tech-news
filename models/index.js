f23625d6f76ed392ef64915692b8f058a9d0acf7
Thor40 <nicholas.kosik@gmail.com>
2020-06-13 12:26:28 -0500

    vote feature final

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 // create associations
 // Post model belongs to User model
@@ -12,4 +13,33 @@ Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
-module.exports = { User, Post };
+// votes on posts belong to user using a foreign key constraint
+User.belongsToMany(Post, {
+    through: Vote,
+        as: 'voted_posts',
+        foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    through: Vote,
+        as: 'voted_posts',
+        foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+Vote.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Vote.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
